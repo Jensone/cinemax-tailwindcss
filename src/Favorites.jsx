d781d1771924ccb01ed09cef34e56dd5c3b9fa48
Jensone <hello@jensone.com>
2024-06-03 15:58:43 +0200

    Connect the database file

--- src/Favorites.jsx
+++ src/Favorites.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Card from "./components/Card";
+import Database from "./assets/database.json";
 
 export default function Favorites() {
+  const [movies, setMovies] = useState(Database); // Initialisation de l'état 'movies' avec la liste par défaut
+
   return (
     <>
       <Navbar />
-      <h1>Favorites</h1>
+      <h1 className="text-center text-3xl font-bold mt-10 mb-5">Favorites</h1>
+      {movies.length > 0 ? (
+        <Card movies={movies} />
+      ) : (
+        <p className="text-center text-2xl font-bold mt-10">
+          No favorite movie, add one for begin
+        </p>
+      )}
       <Footer />
     </>
   );
