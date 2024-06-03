25efb463ef6c634d2f0e6f0096e246d397164fac
Jensone <hello@jensone.com>
2024-06-03 12:45:43 +0200

    Import LoginForm components

--- src/Account.jsx
+++ src/Account.jsx
@@ -1,12 +1,12 @@
-import React from "react";
 import Navbar from "./components/Navbar";
+import LoginForm from "./components/LoginForm";
 import Footer from "./components/Footer";
 
 export default function Account() {
   return (
     <>
       <Navbar />
-      <h1>Account</h1>
+      <LoginForm />
       <Footer />
     </>
   );
